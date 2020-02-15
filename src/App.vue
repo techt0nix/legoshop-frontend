ea99d9dfcd32c635d4423f30ae6eff1d101ce9da
techt0nix <flatleywalker@yandex.ru>
2020-02-15 17:03:27 +0300

    start page is almost done

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="example">
     <myheader></myheader>
     <router-view></router-view>   
+    <myfooter></myfooter>
   </div>
 </template>
 
 <script>
 import myheader from './components/myheader.vue'
+import myfooter from './components/myfooter.vue'
 
 export default {
      name: 'app',
      components: {
-       myheader
+       myheader,
+       myfooter
      }
 }
 </script>
--- src/components/start.vue
+++ src/components/start.vue
@@ -1,32 +1,98 @@
 <template>
-  <div>
-    <img src="../assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <!--about-starts-->
+        <div class="about"> 
+            <div class="container">
+                <div class="about-top grid-1">
+                    <div class="col-md-4 about-left">
+                        <figure class="effect-bubba">
+                            <img class="img-responsive" src="/src/assets/static/images/abt-1.jpg" alt=""/>
+                            <figcaption>
+                                <h2>Nulla maximus nunc</h2>
+                                <p>In sit amet sapien eros Integer dolore magna aliqua</p>	
+                            </figcaption>			
+                        </figure>
+                    </div>
+                    <div class="col-md-4 about-left">
+                        <figure class="effect-bubba">
+                            <img class="img-responsive" src="/src/assets/static/images/abt-2.jpg" alt=""/>
+                            <figcaption>
+                                <h4>Mauris erat augue</h4>
+                                <p>In sit amet sapien eros Integer dolore magna aliqua</p>	
+                            </figcaption>			
+                        </figure>
+                    </div>
+                    <div class="col-md-4 about-left">
+                        <figure class="effect-bubba">
+                            <img class="img-responsive" src="/src/assets/static/images/abt-3.jpg" alt=""/>
+                            <figcaption>
+                                <h4>Cras elit mauris</h4>
+                                <p>In sit amet sapien eros Integer dolore magna aliqua</p>	
+                            </figcaption>			
+                        </figure>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+	    <!--about-end-->
+        <!--product-starts-->
+        <div class="product"> 
+            <div class="container">
+                <div class="product-top">
+                    <div class="product-one" v-for="category in categories">
+                        <div class="col-md-3 product-left">
+                            <div class="product-main simpleCart_shelfItem">
+                                <router-link :to="{ path: 'parts', query: {category: category.id}}" class="mask"> <img class="img-responsive zoom-img" img :src="category.imageName"  alt="" />  </router-link>
+                                <div class="product-bottom">
+                                    <h3>{{ category.engName }}</h3>
+                                    <p>{{ category.rusName }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--product-end-->
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'start',
-    data () {
-      return {
-        msg: 'Welcome to Your Vue.js App'
-      }
+    import {AXIOS} from './http-common'
+    import {ADDRESS} from './backend-address'
+
+    export default {
+        name: 'start',
+
+        data () {
+            return {
+                categories: []
+            }
+        },
+
+        methods: {
+            fetchCategories() {
+              AXIOS.get('/categories',{params: {}})
+              .then(res => {
+                  console.log(res)
+                  let items = res.data
+
+                  for (let i in items) {
+                      items[i].imageName = ADDRESS + items[i].imageName
+                  }
+                
+                  this.$data.categories = items
+              })
+              .catch(err => {
+                  console.error(err); 
+              })  
+            }
+        },
+
+        mounted() {
+            this.fetchCategories();
+        }  
     }
-  }
 </script>
 
